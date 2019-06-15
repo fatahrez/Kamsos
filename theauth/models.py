f66a9e042a533550f57dbeab7f7fbf4a2896f95a
fatahrez <fertahrez@gmail.com>
2019-06-15 11:56:55 +0300

    in theauth/models.py add model User which extends AbstractUser and is set to 3 types of user pastoralist, agrovet and vet

--- theauth/models.py
+++ theauth/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 from django.db.models.signals import post_save
 from rest_framework.authtoken.models import Token
 from django.dispatch import receiver
 
 # Create your models here.
+class User(AbstractUser):
+    is_pastoralist = models.BooleanField(default=False)
+    is_agrovet = models.BooleanField(default=False)
+    is_vet = models.BooleanField(default=False)
+
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(request, instance=None, created=False, **kwargs):
     if created:
