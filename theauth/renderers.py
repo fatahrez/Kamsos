88965c68bdf54685ed51459962b6181cde9b0809
fatahrez <fertahrez@gmail.com>
2019-06-28 09:18:10 +0300

    in theauth.renderers.py add UserJSONRenderer to render data for the different user types

--- theauth/renderers.py
+++ theauth/renderers.py
@@ -23,3 +23,17 @@ class ApiJSONRenderer(JSONRenderer):
             return json.dump({
                 self.object_label: data
             })
+
+class UserJSONRenderer(ApiJSONRenderer):
+    charset = 'utf-8'
+    object_label = 'user'
+    pagination_object_label = 'users'
+    pagination_object_count = 'UsersCount'
+
+    def renderer(self, data, media_type=None, renderer_context=None):
+        token = data.get('token', None)
+
+        if token is not None and isinstance(token, bytes):
+            data['token'] = token.decode('utf-8')
+
+        return super(UserJSONRenderer, self).render(data)
