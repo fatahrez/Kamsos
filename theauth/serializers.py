aaf1e03e35829da9f2d7851e9739ce044e9dd0f1
fatahrez <fertahrez@gmail.com>
2019-06-28 14:54:28 +0300

    update code

--- theauth/serializers.py
+++ theauth/serializers.py
@@ -19,3 +19,33 @@ class PastoralistRegistrationSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return Pastoralist.objects.create_pastoralist(**validated_data)
+
+class AgrovetRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        max_length=128,
+        min_length=8,
+        write_only=True
+    )
+    token = serializers.CharField(max_length=255, read_only=True)
+
+    class Meta:
+        model = Agrovet
+        fields = '__all__'
+
+    def create(self, validated_data):
+        return Agrovet.objects.create_agrovet(**validated_data)
+
+class VetRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        max_length=128,
+        min_length=8,
+        write_only=True
+    )
+    token = serializers.CharField(max_length=255, read_only=True)
+
+    class Meta:
+        model = Vet
+        fields = '__all__'
+
+    def create(self, validated_data):
+        return Vet.objects.create_vet(**validated_data)
