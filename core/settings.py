9aa0251de1104397c986a6b0eaf1720cc505d783
fatahrez <fertahrez@gmail.com>
2019-06-29 02:22:24 +0300

    finish writing login serializer and defining default authentication classes

--- core/settings.py
+++ core/settings.py
@@ -47,7 +47,9 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES':(
-        'rest_framework.authentication.TokenAuthentication',
+        'theauth.backends.JWTPastoralistAuthentication',
+        'theauth.backends.JWTAgrovetAuthentication',
+        'theauth.backends.JWTVetAuthentication',
     )
 }
 
--- theauth/serializers.py
+++ theauth/serializers.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import authenticate
+
 from rest_framework import serializers
 
 from .models import (
@@ -49,3 +51,50 @@ class VetRegistrationSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return Vet.objects.create_vet(**validated_data)
+
+class UserLoginSearilizer(serializers.Serializer):
+    email = serializers.CharField(max_length=255)
+    password = serializers.CharField(max_length=128, write_only=True)
+    token = serializers.CharField(max_length=255, read_only=True)
+
+    def validate(self, data):
+        email = data.get('email', None)
+        password = data.get('password', None)
+
+        user = authenticate(username=email, password=password)
+
+        if user is None:
+            raise serializers.ValidationError(
+                'A user with email and password is required to login'
+            )
+
+        try:
+            user_obj = Pastoralist.objects.get(email=user.email)
+        except Pastoralist.DoesNotExist:
+            user_obj = None
+
+        try:
+            if user_obj is None:
+                user_obj = Agrovet.objects.get(email=user.email)
+        except Agrovet.DoesNotExist:
+            raise serializers.ValidationError(
+                'User with this email and password does not exist'
+            )
+
+        try:
+            if user_obj is None:
+                user_obj = Vet.objects.get(email=user.email)
+        except Vet.DoesNotExist:
+            raise serializers.ValidationError(
+                'User with this email and password does not exist'
+            )
+
+        if not user.is_active:
+            raise serializers.ValidationError(
+                'This user has been deactivated'
+            )
+
+        return{
+            'email': user.email,
+            'token': user.token
+        }
