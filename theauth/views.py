76151ae91e784764e0d56127238be0339d537df4
fatahrez <fertahrez@gmail.com>
2019-06-28 09:35:53 +0300

    in theauth.views add PastoralistRegistration() which is APIView class with permissions, serializers and renderer_classes

--- theauth/views.py
+++ theauth/views.py
@@ -2,8 +2,22 @@ from rest_framework.views import APIView
 from rest_framework.generics import RetrieveUpdateAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
+from rest_framework import status
+
+from .renderers import UserJSONRenderer
+from .serializers import (
+    PastoralistRegistrationSerializer, AgrovetRegistrationSerializer, VetRegistrationSerializer,
+    UserLoginSearilizer
+)
 
 # Create your views here.
 class PastoralistRegistration(APIView):
     permission_classes = (AllowAny,)
-    
+    renderer_classes = (UserJSONRenderer,)
+    serializer_class = PastoralistRegistrationSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
