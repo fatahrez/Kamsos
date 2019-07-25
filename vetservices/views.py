ee1e87060b6fa2df0eb5a1b5fb697cedd5268731
fatahrez <fertahrez@gmail.com>
2019-07-26 00:22:03 +0300

    in vetservices/views.py write VetViewSet with Vet Model and VetSerializer to get all Vets in database

--- vetservices/views.py
+++ vetservices/views.py
@@ -1,4 +1,8 @@
 from theauth.models import Vet
 from rest_framework import viewsets
+from .serializers import VetSerializer
 
 # Create your views here.
+class VetViewSet(viewsets.ModelViewSet):
+    queryset = Vet.objects.all()
+    serializer_class = VetSerializer
