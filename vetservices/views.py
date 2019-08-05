edec9100f79753750f4c988932b999cc957e8722
fatahrez <fertahrez@gmail.com>
2019-08-06 01:11:23 +0300

    in vetservices/views.py add class CustomPermissionForPastoralist() to limit their auth

--- vetservices/views.py
+++ vetservices/views.py
@@ -1,9 +1,20 @@
 from theauth.models import Vet
 from rest_framework import viewsets
 from .serializers import VetSerializer
+from rest_framework import status, permissions
+
 
 # Create your views here.
-# class CustomPermission
+class CustomPermissionForPastoralist(permissions.BasePermission):
+
+    def __init__(self, allowed_methods):
+        self.allowed_methods = allowed_methods
+
+    def has_permission(self, request, view):
+        if 'pastoralist_id' in request.session.keys:
+            return request.method in self.allowed_methods
+
+
 class VetViewSet(viewsets.ModelViewSet):
     queryset = Vet.objects.all()
     serializer_class = VetSerializer
