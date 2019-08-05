e9e0240ca7c866b24ab95b95aa0d18d7e6ea2b39
fatahrez <fertahrez@gmail.com>
2019-08-06 01:03:46 +0300

    create models for order of vet and migrate database

--- vetservices/models.py
+++ vetservices/models.py
@@ -1,3 +1,11 @@
+import uuid
 from django.db import models
+from theauth.models import Pastoralist, Vet
+
 
 # Create your models here.
+class OrderVet(models.Model):
+    pastoralist_id = models.ForeignKey(Pastoralist, on_delete=models.CASCADE)
+    vet_id = models.ForeignKey(Vet, on_delete=models.CASCADE)
+    booked_time = models.DateTimeField(auto_now_add=True)
+    order_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
--- vetservices/views.py
+++ vetservices/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets
 from .serializers import VetSerializer
 
 # Create your views here.
+# class CustomPermission
 class VetViewSet(viewsets.ModelViewSet):
     queryset = Vet.objects.all()
     serializer_class = VetSerializer
