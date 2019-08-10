d793264fbf91f8cfbd4ebf0130e38314a0197f6d
fatahrez <fertahrez@gmail.com>
2019-08-10 17:54:18 +0300

    in vetservices/views add class-based view RequestVet to post for requesting a Vet and witj serializer RequestVetSerializer

--- vetservices/serializers.py
+++ vetservices/serializers.py
@@ -1,7 +1,12 @@
 from theauth.models import Vet
 from rest_framework import serializers
 
+
 class VetSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Vet
-        fields = ["id","first_name", "middle_name", "last_name", "username", "email"]
+        fields = ["id", "first_name", "middle_name", "last_name", "username", "email", "telephone_number", "county",
+                  "sub_county", "qualifications", "vet_image"]
+class RequestVetSerializer(serializers.Serializer):
+    pass
--- vetservices/views.py
+++ vetservices/views.py
@@ -1,6 +1,11 @@
+from functools import partial
+
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from theauth.models import Vet
 from rest_framework import viewsets
-from .serializers import VetSerializer
+from .serializers import VetSerializer, RequestVetSerializer
 from rest_framework import status, permissions
 
 
@@ -18,3 +23,22 @@ class CustomPermissionForPastoralist(permissions.BasePermission):
 class VetViewSet(viewsets.ModelViewSet):
     queryset = Vet.objects.all()
     serializer_class = VetSerializer
+
+
+class RequestVet(APIView):
+    serializer_class = RequestVetSerializer
+    permission_classes = (partial(CustomPermissionForPastoralist, ['GET', 'HEAD', 'POST']),)
+
+    def post(self, request, format=None):
+        context = {
+            'pastoralist_id': request.session['pastoralist_id'],
+            'vet_id': request.session['vet_id']
+        }
+        serializer = RequestVetSerializer(data=request.data, context=context)
+        if serializer.is_valid():
+            serializer.save()
+            data = {
+                "success": "You have set an appointment successfully"
+            }
+            return Response(data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
