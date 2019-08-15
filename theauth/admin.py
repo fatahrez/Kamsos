3e22888b6dc59fdc24ba3ef335a5737be223358c
fatahrez <fertahrez@gmail.com>
2019-08-15 22:41:43 +0300

    in theauth/admin.py register Pastoralist, Vet, Agrovet, User to the django-admin

--- theauth/admin.py
+++ theauth/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Pastoralist, Vet, Agrovet, User
 
 # Register your models here.
+admin.site.register(Pastoralist)
+admin.site.register(Vet)
+admin.site.register(Agrovet)
+admin.site.register(User)
