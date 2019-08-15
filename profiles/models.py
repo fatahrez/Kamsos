b939aa567b8cd66a63280825b506cd610e6063cc
fatahrez <fertahrez@gmail.com>
2019-08-15 23:25:45 +0300

    in profiles/models.py create model Profile for use for user profile

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(
+        'theauth.User', on_delete=models.CASCADE
+    )
+
+    image = models.ImageField(blank=True)
+
+    def __str__(self):
+        return self.user.username
