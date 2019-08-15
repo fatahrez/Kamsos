b49a99466f514dac0593f75c400ecab74404de04
fatahrez <fertahrez@gmail.com>
2019-08-15 22:32:50 +0300

    in theauth/models.py in class UserManager add methods create_user and create_superuser to create a superuser

--- theauth/models.py
+++ theauth/models.py
@@ -13,6 +13,30 @@ from django.contrib.auth.models import BaseUserManager
 
 # Create your models here.
 class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        # if username is None:
+        #     raise TypeError('Users must have a username')
+
+        if email is None:
+            raise TypeError('User must have email.')
+
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, email, password):
+        if password is None:
+            raise TypeError('Superusers must have a password')
+
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
+
     def get_by_natural_key(self, email):
         return self.get(email=email)
 
