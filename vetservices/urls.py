5d5f01a26a375fc9c3e960cf432b5971e9ad4b40
fatahrez <fertahrez@gmail.com>
2019-08-15 21:26:06 +0300

    in vetservices/urls.py add urls for requestvet

--- vetservices/urls.py
+++ vetservices/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import url, include
 from rest_framework import routers
 from . import views
 
-router = routers.DefaultRouter()
-router.register(r'', views.VetViewSet)
-router.register(r'requestvet', views.RequestVet.as_view())
+# router = routers.DefaultRouter()
+# router.register(r'', views.VetViewSet)
+# router.register(r'requestvet', views.RequestVet.as_view())
 
 urlpatterns = [
-    url(r'', include(router.urls)),
-    # url(r'requestvet', views.RequestVet.as_view(), name='request-vet')
+    url(r'vets/', views.VetViewSet.as_view(), name="getAllVets"),
+    url(r'requestvet/', views.RequestVet.as_view(), name='request-vet')
 ]
