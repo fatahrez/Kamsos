c7978302a6d6950c1f392a0f9d9c6a7788090f18
fatahrez <fertahrez@gmail.com>
2019-09-01 02:45:21 +0300

    in vetservices view add viewset for requesting and showing all vets

--- vetservices/urls.py
+++ vetservices/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import url, include
 from rest_framework import routers
+
+from theauth.models import Vet
+from vetservices.serializers import VetSerializer
 from . import views
 
-# router = routers.DefaultRouter()
-# router.register(r'', views.VetViewSet)
-# router.register(r'requestvet', views.RequestVet.as_view())
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'vets', views.VetViewSet)
 
 urlpatterns = [
-    url(r'vets/', views.VetViewSet.as_view(), name="getAllVets"),
-    url(r'requestvet/', views.RequestVet.as_view(), name='request-vet')
+    url(r'', include(router.urls)),
+    url(r'vets/(?P<vet_slug>[-\w]+)/request/?$', views.RequestVetCreateAPIView.as_view(), name='request-vet')
 ]
