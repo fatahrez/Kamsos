0f45a31a819abe289909516325b7f47dac29e419
fatahrez <fertahrez@gmail.com>
2019-08-31 06:20:14 +0300

    modify OrderVet model in vetservices/models.py

--- vetservices/models.py
+++ vetservices/models.py
@@ -1,11 +1,13 @@
 import uuid
 from django.db import models
+
+from profiles.models import Profile
 from theauth.models import Pastoralist, Vet
 
 
 # Create your models here.
 class OrderVet(models.Model):
-    pastoralist_id = models.ForeignKey(Pastoralist, on_delete=models.CASCADE)
-    vet_id = models.ForeignKey(Vet, on_delete=models.CASCADE)
-    booked_time = models.DateTimeField(auto_now_add=True)
+    pastoralist_id = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
+    vet_id = models.ForeignKey(Vet, on_delete=models.CASCADE, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     order_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
