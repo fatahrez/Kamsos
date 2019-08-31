ecf0afcc44b5a6e6aa609c47d47487f57b262b19
fatahrez <fertahrez@gmail.com>
2019-08-31 06:22:32 +0300

    modify vetservices/serializers.py rewrite VetSerializer and RequestVetSerializer to make them dynamic

--- vetservices/serializers.py
+++ vetservices/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework.exceptions import ValidationError
 
+from profiles.serializers import ProfileSerializer
 from theauth.models import Vet, Pastoralist
 from rest_framework import serializers
 
@@ -7,34 +8,30 @@ from vetservices.models import OrderVet
 
 
 class VetSerializer(serializers.ModelSerializer):
+    slug = serializers.SlugField(required=False)
+
     class Meta:
         model = Vet
         fields = ["id", "first_name", "middle_name", "last_name", "username", "email", "telephone_number", "county",
                   "sub_county", "qualifications", "vet_image"]
 
 
-class RequestVetSerializer(serializers.Serializer):
-    vet_id = serializers.CharField()
+class RequestVetSerializer(serializers.ModelSerializer):
+    pastoralist_id = ProfileSerializer(required=False)
 
-    def validate(self, data):
-        vet_id = data.get("vet_id")
+    createdAt = serializers.SerializerMethodField(method_name='get_created_at')
 
-        if not vet_id:
-            raise ValidationError("Vet does not exist")
-        try:
-            vet = Vet.objects.get(pk=vet_id)
-        except Vet.DoesNotExist:
-            raise ValidationError("Vet doesn't exist")
+    # vet_id = serializers.CharField()
 
-        return data
+    class Meta:
+        model = OrderVet
+        fields = (
+            'id', 'pastoralist_id', 'vet_id', 'createdAt'
+        )
 
     def create(self, validated_data):
-        vet_id = validated_data.pop('vet_id')
-        pastoralist_id = self.context.get("pastoralist_id")
-        pastoralist = Pastoralist.objects.get(pk=pastoralist_id)
-        vet = Vet.objects.get(pk=vet_id)
-        obj1 = OrderVet()
-        obj1.pastoralist_id = pastoralist
-        obj1.vet_id = vet
-        obj1.save()
-        return obj1
+        pastoralist_id = self.context['pastoralist_id']
+        vet_id = self.context['vet_id']
+
+    def get_created_at(self, instance):
+        return instance.created_at.isoformat
